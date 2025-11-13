74fd02b9b2366a0fb69e2387a5b7ae6ec0ad692a
manhtthe161599 <manhtthe161599@fpt.edu.vn>
2025-11-13 22:04:25 +0700

    fix bug

--- src/main/java/com/web/bookingKol/domain/user/dtos/BookedPackageResponse.java
+++ src/main/java/com/web/bookingKol/domain/user/dtos/BookedPackageResponse.java
@@ -17,8 +17,7 @@ public class BookedPackageResponse {
     private String campaignName;
     private String campaignStatus;
     private String objective;
-    private BigDecimal budgetMin;
-    private BigDecimal budgetMax;
+    private BigDecimal targetPrice;
     private Instant startDate;
     private Instant endDate;
     private String recurrencePattern;
